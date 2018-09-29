b7e242170c811018221245497be44c52f43ab324
Vinod M <32615629+VinodMvd@users.noreply.github.com>
2018-09-30 02:47:53 +0530

    Updated

--- Js/script.js
+++ Js/script.js
@@ -8,6 +8,19 @@ app.controller('weatherCtrl', function($scope, $http){
 			var apiKey = "f1f9d146fbf71bcd80eeabab821f34cb";
 			console.log(vm.lat);
 		});*/
+		var count = 0;
+    	navigator.geolocation.getCurrentPosition(function(position) {
+			var lat = position.coords.latitude;
+			var lng = position.coords.longitude;
+			getthevalue(lat,lng);
+			count = 1;
+		},
+		function (error) { 
+  			if (error.code == error.PERMISSION_DENIED)
+      		alert("Weather might not be accurate as location is blocked");
+	  		getthevalue();
+		});
+		function getthevalue(a,b){
 		$http({
 			method:"get",
 			url: "https://api.ipify.org/?format=json"
@@ -21,28 +34,32 @@ app.controller('weatherCtrl', function($scope, $http){
 		}).then(function(data){
 				vm.lat = data.data.latitude;
 				vm.lon = data.data.longitude;
-				var apiKey = "17ca9be1b3c6b2f28c31fa0c7c6dafcd";
-				
-				var openWeatherUrl = "https://api.openweathermap.org/data/2.5/find?lat="+vm.lat+"&lon="+vm.lon+"&appid="+apiKey;
-				
+				var apiKey = "17ca9be1b3c6b2f28c31fa0c7c6dafcd";	
+				if(count == 1){
+					var openWeatherUrl = "https://api.openweathermap.org/data/2.5/find?lat="+a+"&lon="+b+"&appid="+apiKey;
+				}
+				else{
+					var openWeatherUrl = "https://api.openweathermap.org/data/2.5/find?lat="+vm.lat+"&lon="+vm.lon+"&appid="+apiKey;
+				}
+			
 			$http({
 				method:"get",
 				url: openWeatherUrl
 			}).then(function(data){
 				console.log(openWeatherUrl);
-				vm.description = data.data.list[2].weather[0].description;
-				vm.speed = (2.237*data.data.list[2].wind.speed).toFixed(1)+"Mph";
-				vm.location = data.data.list[2].name;
-				vm.temp=data.data.list[2].main.temp;
+				vm.description = "scattered clouds";
+				vm.speed = (2.237*data.data.list[0].wind.speed).toFixed(1)+"Mph";
+				vm.location = data.data.list[0].name;
+				vm.temp=data.data.list[0].main.temp;
 				vm.fTemp = (vm.temp*(9/5)-459.67).toFixed(1)+"°F";
 				vm.cTemp = (vm.temp-273).toFixed(1)+"°C";
-				vm.humidity = data.data.list[2].main.humidity;
-				vm.icon = "https://openweathermap.org/img/w/"+data.data.list[2].weather[0].icon+".png";
+				vm.humidity = data.data.list[0].main.humidity;
+				vm.icon = "https://openweathermap.org/img/w/"+data.data.list[0].weather[0].icon+".png";
 				
 				switch(vm.description){
 					case ('scattered clouds'):{
 						vm.weatherBackground = {
-							"background": "url('Images/Scattered Clouds.jpg')",
+							"background": "url('Images/Scattered clouds.jpg')",
 							"background-size": "cover",
 							"background-repeat": "no-repeat"
 						}
@@ -80,6 +97,14 @@ app.controller('weatherCtrl', function($scope, $http){
 						}
 					break;
 					}
+					case ('overcast clouds'):{
+						vm.weatherBackground = {
+							"background": "url('Images/Overcast Clouds.jpg')",
+							"background-size": "cover",
+							"background-repeat": "no-repeat"
+						}
+					break;
+					}
 					case ('broken clouds'):{
 						vm.weatherBackground = {
 							"background": "url('Images/Broken Clouds.jpg')",
@@ -99,14 +124,6 @@ app.controller('weatherCtrl', function($scope, $http){
 					case ('sky is clear'):{
 						vm.weatherBackground = {
 							"background": "url('Images/Clear Sky.jpg')",
-							"background-size": "cover",
-							"background-repeat": "no-repeat"
-						}
-					break;
-					}
-					case ('overcast clouds'):{
-						vm.weatherBackground = {
-							"background": "url('Images/Overcast Clouds.jpg')",
 							"background-size": "cover",
 							"background-repeat": "no-repeat"
 						}
@@ -172,7 +189,7 @@ app.controller('weatherCtrl', function($scope, $http){
 				$scope.checktype=true;
 			}
 		}
-		
+			}
 });
 $(function() {
     $('#toggle-one').bootstrapToggle();
@@ -189,4 +206,8 @@ document.onreadystatechange = function () {
       },1000);
   }
 }
+
+
+
+
 
